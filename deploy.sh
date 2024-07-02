4350ddbe880c2bdd0370f34e6f0c3cfa47f5f271
ranjithkumr77 <108941104+ranjithkumr77@users.noreply.github.com>
2024-07-02 13:36:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
- DEV_REPO = 'ranjith7777/dev'
- PROD_REPO = 'ranjith7777/prod'
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
-    sh "docker tag ranjith ${DEV_REPO}:latest"
+    sh "docker tag ranjith ranjith7777/dev:latest"
     sh " docker login -u ranjith7777 --password dckr_pat_IBO-I29lL9Bt-Q5OxmdoXxIwwk0"
-    sh "docker push ${DEV_REPO}:latest"
+    sh "docker push ranjith7777/dev:latest"
 elif [[ $GIT_BRANCH == "origin/master" ]]; then
     # Build your project
     sh'chmod +x build.sh'
