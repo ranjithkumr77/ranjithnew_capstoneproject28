f7c195c7cb16c03f8dbcad07d5fec91b2811d0ce
ranjithkumr77 <108941104+ranjithkumr77@users.noreply.github.com>
2024-07-02 14:04:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ elif [[ $GIT_BRANCH == "origin/master" ]]; then
     # Build your project
     sh'chmod +x build.sh'
     sh'./build.sh' 
-    sh "docker tag ranjith ${PROD_REPO}:latest"
-    sh " docker login -u ranjith7777 --password dckr_pat_IBO-I29lL9Bt-Q5OxmdoXxIwwk0"
-    sh "docker push ${PROD_REPO}:latest"
+    docker tag ranjith ranjith7777/prod:latest
+    docker login -u ranjith7777 --password dckr_pat_IBO-I29lL9Bt-Q5OxmdoXxIwwk0
+    docker push ranjith7777/prod:latest
 fi
 docker-compose up -d
