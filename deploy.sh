69e891d526b7812e15158cc8d84d993c94c18296
ranjithkumr77 <108941104+ranjithkumr77@users.noreply.github.com>
2024-07-02 14:31:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,9 +12,9 @@ elif [[ $GIT_BRANCH == "dev" ]]; then
     # Build your project
     sh'chmod +x build.sh'
     sh'./build.sh' 
-    docker tag ranjith ranjith7777/prod:latest
+    docker tag ranjith ranjith7777/dev:latest
     docker login -u ranjith7777 --password dckr_pat_IBO-I29lL9Bt-Q5OxmdoXxIwwk0
-    docker push ranjith7777/prod:latest
+    docker push ranjith7777/dev:latest
 fi
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "$GIT_BRANCH"
@@ -25,7 +25,7 @@ if [[ $GIT_BRANCH == "dev" ]]; then
      docker tag ranjith ranjith7777/dev:latest
      docker login -u ranjith7777 --password dckr_pat_IBO-I29lL9Bt-Q5OxmdoXxIwwk0
      docker push ranjith7777/dev:latest
-elif [[ $GIT_BRANCH == "master" ]]; then
+elif [[ $GIT_BRANCH == "dev" ]]; then
     # Build your project
     sh'chmod +x build.sh'
     sh'./build.sh' 
