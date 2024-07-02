474eacbf3c7eed54f4bc3ffc70e1cdd345a944cb
ranjithkumr77 <108941104+ranjithkumr77@users.noreply.github.com>
2024-07-02 14:17:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "$GIT_BRANCH"
-if [[ $GIT_BRANCH == "orgin/master" ]]; then
+if [[ $GIT_BRANCH == "master" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
      docker tag ranjith ranjith7777/prod:latest
      docker login -u ranjith7777 --password dckr_pat_IBO-I29lL9Bt-Q5OxmdoXxIwwk0
      docker push ranjith7777/prod:latest
-elif [[ $GIT_BRANCH == "origin/master" ]]; then
+elif [[ $GIT_BRANCH == "master" ]]; then
     # Build your project
     sh'chmod +x build.sh'
     sh'./build.sh' 
